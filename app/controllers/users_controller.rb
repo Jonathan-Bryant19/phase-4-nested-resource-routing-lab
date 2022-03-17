85e5047177b23b1c2ccd63d314ad9c3d97244c9c
Jonathan Bryant <spectrum19tech@gmail.com>
2022-03-17 09:57:53 -0700

    commit tree 00d69e6c6b9fcb5a64c3f13d96ed09d6c4b2f7ef

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,22 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def show
-    user = User.find_by(id: params[:id])
-    render json: user, include: :items
+    user = find_user
+    if user
+      render json: user, include: :items
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
   end
+
+  private
+
+  def find_user
+    User.find_by(id: params[:id])
+  end
+
+
+
 
 end
